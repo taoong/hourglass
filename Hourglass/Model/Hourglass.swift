795a2b7105dc4285d54901aa5d18ecfc23e071e6
Tao Ong <taoong@berkeley.edu>
2017-11-28 14:58:54 -0800

    starting files

---
+++ Hourglass/Model/Hourglass.swift
@@ -0,0 +1,25 @@
+//
+//  hourglass.swift
+//  Hourglass
+//
+//  Created by Tao Ong on 11/24/17.
+//  Copyright © 2017 Tao Ong. All rights reserved.
+//
+
+import Foundation
+
+class Hourglass {
+    
+    var productive : Timer
+    var unproductive : Timer
+    
+    init () {
+        self.productive = Timer.init()
+        self.unproductive = Timer.init()
+    }
+    
+    func changeTimer() -> Void {
+        
+    }
+    
+}
